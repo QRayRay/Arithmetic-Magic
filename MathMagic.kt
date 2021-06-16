a55909299d8378c3edea69effad62d8bafcf2732
Raymond C Quintana <77255338+QRayRay@users.noreply.github.com>
2021-06-15 19:15:51 -0700

    Create MathMagic.kt

---
+++ MathMagic.kt
@@ -0,0 +1,16 @@
+// This program determines the user's age using  an arithmetic magic trick. 
+fun main() {
+val userName = "Raymond"
+var magicNum: Int
+ magicNum = 2 * 5 
+
+    magicNum += 3 
+
+    magicNum *= 2 
+
+    magicNum += 5
+
+    magicNum -= 4
+    
+    println("$userName's age is $magicNum") 
+}
